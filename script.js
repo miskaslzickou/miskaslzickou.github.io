00f25493c49a32daeecc60bb41e6ca85a7776c54
miskaslzickou <ilikemoonm@gmail.com>
2025-05-03 12:19:59 +0200

    Some cool transitions

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 import { gsap } from "https://cdn.skypack.dev/gsap";
 
 gsap.from("#welcome", { x: 0, opacity: 0, duration: 1 });
+const text = "Jsem programátor se zkušenostmi v jazycích C#, JavaScript a práci s databázemi. Neustále se učím a rozvíjím své schopnosti v moderních technologiích.";
+let index = 0;
+// gsap.from(".skill-box", {
+//   opacity: 0,
+//   y: -50,
+//   duration: 0.75,
+ 
+// });
+
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("intro-text").textContent += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 50); // Rychlost psaní
+  }
+}
+
+typeWriter();
